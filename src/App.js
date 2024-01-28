3e31b3a864dcd29330e7d84b449b4008646b4d87
Nooshin0962 <nooha.0962@gmail.com>
2024-01-28 20:23:15 +0330

    delete logo.svg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <h1>
       Weather App
+      </h1>
+      <footer>
+      <p>
+        This code is <a href="https://github.com/Nooshin0962/react-weather-app" target="_blank" rel="noreferrer">open source</a> by Nooshin Semsarilar
+      </p>
+      </footer>
     </div>
   );
 }
