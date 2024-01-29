943f874ba8c5148c549090ae50b6d501f11ded18
Nooshin0962 <nooha.0962@gmail.com>
2024-01-29 07:13:16 +0330

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>
-      Weather App
-      </h1>
-      <footer>
-      <p>
-        This code is <a href="https://github.com/Nooshin0962/react-weather-app" target="_blank" rel="noreferrer">open source</a> by Nooshin Semsarilar
-      </p>
-      </footer>
+      <div className="container">
+        <h1>
+          Weather App
+        </h1>
+        <footer>
+          <p>
+           This code is <a href="https://github.com/Nooshin0962/react-weather-app" target="_blank" rel="noreferrer">open source</a> by Nooshin Semsarilar
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
