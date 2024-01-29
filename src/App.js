db322d879b7001fae213dbd4ac671d89dcae805d
Nooshin0962 <nooha.0962@gmail.com>
2024-01-29 10:00:51 +0330

    HTML

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>
-          Weather App
-        </h1>
+          <Weather />
         <footer>
           <p>
            This code is <a href="https://github.com/Nooshin0962/react-weather-app" target="_blank" rel="noreferrer">open source</a> by Nooshin Semsarilar
